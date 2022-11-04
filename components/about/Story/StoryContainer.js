fb8af953d51c12322bcd960f854931f421e60a11
latz09 <85132328+latz09@users.noreply.github.com>
2022-11-04 15:05:38 -0500

    end of day

--- components/about/Story/StoryContainer.js
+++ components/about/Story/StoryContainer.js
@@ -1,14 +1,35 @@
 import Paragraph from './Paragraph';
 import { paragraphs } from '../../../data/ourStory';
-
+import { motion } from 'framer-motion';
 const StoryContainer = () => {
 	return (
 		<div className='max-w-3xl mx-auto grid gap-2 my-4 px-2 md:px-0'>
-			{paragraphs.map((paragraph) => (
-				<div key={paragraph.id} className="p-4">
-					<Paragraph text={paragraph.text} />
-				</div>
-			))}
+			<h1 className='text-xl font-normal md:text-4xl flex justify-center text-primary text-center py-6'>
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition={{ delay: 0.2, duration: 0.4 }}
+				>
+					{' '}
+					A Hairy Situtation...
+				</motion.div>{' '}
+				<motion.div
+					initial={{ x: '100vw', opacity: 0 }}
+					animate={{ x: 0, opacity: [0, 0, 1] }}
+					transition={{ type: 'spring', delay: 0.7, stiffness: 30 }}
+				>
+					{' '}
+					With a Happy Ending
+				</motion.div>
+			</h1>
+			<div>
+				
+				{paragraphs.map((paragraph) => (
+					<div key={paragraph.id} className='p-4'>
+						<Paragraph text={paragraph.text} />
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
--- pages/our-full-story.js
+++ pages/our-full-story.js
@@ -1,29 +1,18 @@
 import StoryContainer from '../components/about/Story/StoryContainer';
 import { motion } from 'framer-motion';
+import jessica from '../public/images/jessica.jpg';
+import Image from 'next/image';
 
 const OurFullStory = () => {
 	return (
-		<div className='grid bg-gray-200 py-8'>
-			<h1 className='text-xl font-normal md:text-4xl flex justify-center text-primary text-center py-6'>
-				<motion.div
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					transition={{ delay: 0.2, duration: 0.4 }}
-				>
-					{' '}
-					A Hair Situtation...
-				</motion.div>{' '}
-				<motion.div
-					initial={{ x: '100vw', opacity: 0 }}
-					animate={{ x: 0, opacity: [0, 0, 1] }}
-					transition={{ type: 'spring', delay: .7, stiffness: 30}}
-				>
-					{' '}
-					With a Happy Ending
-				</motion.div>
-			</h1>
-			<StoryContainer />
-		</div>
+		<>
+			{/* <div className="grid place-items-center ">
+				<Image src={jessica} alt='' />
+			</div> */}
+			<div className='grid bg-gray-200 py-8 '>
+				<StoryContainer />
+			</div>
+		</>
 	);
 };
 
